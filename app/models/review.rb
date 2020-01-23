3c97642ce67a7f4c84ce3da820909ae7c920f9a1
Tom Hudson <tom.hudson89@gmail.com>
2020-01-23 20:41:35 +0000

    all done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  validates :content, presence: true
   belongs_to :restaurant
+  validates :content, presence: true
   validates :rating, numericality: { only_integer: true }, inclusion: { in: [0, 1, 2, 3, 4, 5] }
 end
